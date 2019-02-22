78727b8c8c416f8d05b319002f12c4d7e2e7aebe
Adrian <adriansamatan@gmail.com>
2019-02-22 18:17:25 +0100

    ficheros iniciales de spring

---
+++ src/main/java/com/unicast/unicast_backend/UnicastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unicast.unicast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicastBackendApplication.class, args);
+	}
+
+}
