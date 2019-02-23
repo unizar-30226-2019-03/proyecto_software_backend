fea0169e9804cb37f246fdb072fb0e6b6bf05036
jmvallejop1 <jmvallejop1@gmail.com>
2019-02-23 12:32:48 +0100

    Prueba de Hola

---
+++ src/main/java/com/unicast/unicast_backend/Hola.java
@@ -0,0 +1,15 @@
+package com.unicast.unicast_backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Hola {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
+
