f9926ec3d86f64066a6b3236d486672552e0fd0e
jmvallejop1 <jmvallejop1@gmail.com>
2019-02-25 19:27:34 +0100

    HotReload falta .classpath

--- src/main/java/com/unicast/unicast_backend/Hola.java
+++ src/main/java/com/unicast/unicast_backend/Hola.java
@@ -8,7 +8,7 @@ public class Hola {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Adri!";
     }
     
 }
