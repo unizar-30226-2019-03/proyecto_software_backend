d4a9e1963ae750af071248a69a22dc5b8412bd87
jmvallejop1 <jmvallejop1@gmail.com>
2019-03-20 16:50:46 +0100

    Lombok funcionando + NotificationCategory

---
+++ src/main/java/com/unicast/unicast_backend/repository/NotificationCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.unicast.unicast_backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.unicast.unicast_backend.model.NotificationCategory;
+
+@RepositoryRestResource(collectionResourceRel = "notificationCategory", path = "notificationCategory")
+public interface NotificationCategoryRepository extends PagingAndSortingRepository<NotificationCategory, Long> {
+
+	List<NotificationCategory> findById(@Param("id") String id);
+}
