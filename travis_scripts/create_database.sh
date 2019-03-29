ced91f7ca3d1cf468e0c5e1ee080d5d46319ee2a
Adrian <adriansamatan@gmail.com>
2019-03-29 16:24:25 +0100

    crear tablas en travis

--- travis_scripts/create_database.sh
+++ travis_scripts/create_database.sh
@@ -3,5 +3,6 @@
 psql -c "CREATE DATABASE unicast;" -U postgres
 psql -c "CREATE USER unicast_backend WITH PASSWORD 'unicast_backend';" -U postgres
 psql -c "GRANT ALL ON SCHEMA public TO unicast_backend;" -U postgres
+psql -U postgres -d unicast -a -f src/main/resources/schema.sql
 psql -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO unicast_backend;" -U postgres
 psql -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO unicast_backend;" -U postgres
