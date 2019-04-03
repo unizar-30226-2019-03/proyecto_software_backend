80b7a810625d3e8435bacd0d0e297da684ca8178
Adrian <adriansamatan@gmail.com>
2019-04-03 22:02:04 +0200

    metodos devuelven lista, no solo uno

--- src/main/java/com/unicast/unicast_backend/persistance/repository/MessageRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/MessageRepository.java
@@ -1,9 +1,11 @@
 package com.unicast.unicast_backend.persistance.repository;
 
+import java.util.List;
+
 import com.unicast.unicast_backend.persistance.model.Message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    Message findByText(String text);
+    List<Message> findByText(String text);
 }
--- src/main/java/com/unicast/unicast_backend/persistance/repository/ReproductionListRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/ReproductionListRepository.java
@@ -1,9 +1,11 @@
 package com.unicast.unicast_backend.persistance.repository;
 
+import java.util.List;
+
 import com.unicast.unicast_backend.persistance.model.ReproductionList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReproductionListRepository extends JpaRepository<ReproductionList, Long> {
-    ReproductionList findByName(String name);
+    List<ReproductionList> findByName(String name);
 }
