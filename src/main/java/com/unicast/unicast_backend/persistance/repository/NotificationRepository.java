c802d7305d5d51661948ad49849a132edd26d9ed
Adrian <adriansamatan@gmail.com>
2019-04-03 22:01:42 +0200

    import sobra

--- src/main/java/com/unicast/unicast_backend/persistance/repository/NotificationRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/NotificationRepository.java
@@ -3,7 +3,6 @@ package com.unicast.unicast_backend.persistance.repository;
 import com.unicast.unicast_backend.persistance.model.Notification;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "notification", path = "notifications")
