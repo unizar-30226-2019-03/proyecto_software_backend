927e3040210b3b2a77b735e22abc610820469cd1
jmvallejop1 <jmvallejop1@gmail.com>
2019-04-17 11:11:13 +0200

    Comentario en s3handler para que pase los test + rest repo en university

--- src/main/java/com/unicast/unicast_backend/persistance/repository/UniversityRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/UniversityRepository.java
@@ -3,7 +3,12 @@ package com.unicast.unicast_backend.persistance.repository;
 import com.unicast.unicast_backend.persistance.model.University;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 
-public interface UniversityRepository extends JpaRepository<University, Long> {
-    University findByName(String name);
+
+@RepositoryRestResource(collectionResourceRel = "university", path = "universities")
+public interface UniversityRepository extends JpaRepository<University, Long>{
 }
--- src/main/java/com/unicast/unicast_backend/s3handlers/S3VideoHandler.java
+++ src/main/java/com/unicast/unicast_backend/s3handlers/S3VideoHandler.java
@@ -9,14 +9,15 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-public final class S3VideoHandler {
+/*public final class S3VideoHandler {
     @Autowired
     private static S3Constants s3Constants;
 
     public static String uploadVideo(MultipartFile uploadedVideo) throws IllegalStateException, IOException {
         File file = new File(uploadedVideo.getOriginalFilename());
         uploadedVideo.transferTo(file);
+    }
 
 
         // TODO: subirlo y tal a s3
-}
+}*/
