8efbb49372325c43104cf1fec4f805b2cacdb443
Adrian <adriansamatan@gmail.com>
2019-04-20 17:25:58 +0200

    subject como repositorio rest

--- src/main/java/com/unicast/unicast_backend/persistance/repository/SubjectRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/SubjectRepository.java
@@ -3,7 +3,9 @@ package com.unicast.unicast_backend.persistance.repository;
 import com.unicast.unicast_backend.persistance.model.Subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "subject", path = "subjects")
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     Subject findByName(String name);
 }
