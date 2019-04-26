1f918518235d278e2effb205d8eccdfb7141f373
Adrian <adriansamatan@gmail.com>
2019-04-26 17:59:49 +0200

    continuacion de documentar api

--- src/main/java/com/unicast/unicast_backend/persistance/repository/UniversityRepository.java
+++ src/main/java/com/unicast/unicast_backend/persistance/repository/UniversityRepository.java
@@ -5,8 +5,7 @@ import com.unicast.unicast_backend.persistance.model.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
-@RepositoryRestResource(collectionResourceRel = "university", path = "universities")
-public interface UniversityRepository extends JpaRepository<University, Long>{
+@RepositoryRestResource(collectionResourceRel = "universities", path = "universities")
+public interface UniversityRepository extends JpaRepository<University, Long> {
     University findByName(String name);
 }
