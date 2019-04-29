b49101c503a9b3e2a976f79a626a5b910c8ff2e5
Adrian <adriansamatan@gmail.com>
2019-04-29 17:55:29 +0200

    eliminar imports innecesarios

--- src/main/java/com/unicast/unicast_backend/persistance/model/Degree.java
+++ src/main/java/com/unicast/unicast_backend/persistance/model/Degree.java
@@ -1,9 +1,7 @@
 package com.unicast.unicast_backend.persistance.model;
 
-import java.sql.Timestamp;
 import java.util.Collection;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
