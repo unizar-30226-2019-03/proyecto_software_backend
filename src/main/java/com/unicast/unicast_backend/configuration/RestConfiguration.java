61ac0c3480e1543a5dad9c2679e218e7917d985f
Adrian <adriansamatan@gmail.com>
2019-05-01 14:04:08 +0200

    ids incluidos en las respuestas ahora

---
+++ src/main/java/com/unicast/unicast_backend/configuration/RestConfiguration.java
@@ -0,0 +1,22 @@
+package com.unicast.unicast_backend.configuration;
+
+import com.unicast.unicast_backend.persistance.model.Subject;
+import com.unicast.unicast_backend.persistance.model.University;
+import com.unicast.unicast_backend.persistance.model.User;
+import com.unicast.unicast_backend.persistance.model.Video;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Video.class);
+        config.exposeIdsFor(University.class);
+        config.exposeIdsFor(Subject.class);
+    }
+}
