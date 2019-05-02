d01991864d750bc41816cd92693fc00a74c09397
Adrian <adriansamatan@gmail.com>
2019-05-02 21:20:11 +0200

    formateo

--- src/main/java/com/unicast/unicast_backend/controllers/SubjectController.java
+++ src/main/java/com/unicast/unicast_backend/controllers/SubjectController.java
@@ -7,54 +7,57 @@ import com.unicast.unicast_backend.persistance.model.Subject;
 import com.unicast.unicast_backend.persistance.model.Video;
 import com.unicast.unicast_backend.persistance.model.Vote;
 import com.unicast.unicast_backend.persistance.repository.SubjectRepository;
+import com.unicast.unicast_backend.persistance.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RepositoryRestController
+@RestController
 public class SubjectController {
 
-    private final SubjectRepository repo;
+    private final SubjectRepository subjectRepository;
+
+    private final UserRepository userRepository;
 
     @Autowired
-    public SubjectController(SubjectRepository u) {
-        this.repo = u;
+    public SubjectController(SubjectRepository u, UserRepository userRepository) {
+        this.subjectRepository = u;
+        this.userRepository = userRepository;
     }
+
     @RequestMapping(method = RequestMethod.GET, value = "/subjects/ranking")
     public @ResponseBody ResponseEntity<?> getRanking() {
-        List<Subject> producers = repo.findAll();
+        List<Subject> producers = subjectRepository.findAll();
         Collection<Video> videos;
         Collection<Vote> votes;
-        for(Subject s: producers){
-            videos=s.getVideos();
-            for(Video v:videos){
-                votes=v.getVotes();
-                for(Vote vo:votes){
+        for (Subject s : producers) {
+            videos = s.getVideos();
+            for (Video v : videos) {
+                votes = v.getVotes();
+                for (Vote vo : votes) {
 
                 }
             }
         }
-        //Obtener los videos y comprobar las puntiaciones para hacer los rankings
+        // Obtener los videos y comprobar las puntiaciones para hacer los rankings
 
-        return ResponseEntity.ok(producers); 
-    
+        return ResponseEntity.ok(producers);
+
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/subjects/points")
     @ResponseBody
-    public Subject getPoints(@RequestParam String name){
+    public Subject getPoints(@RequestParam String name) {
         Subject s;
-        s  = repo.findByName(name);
+        s = subjectRepository.findByName(name);
         s.setPoints(7.2);
         return s;
 
     }
-
-
 
 }
