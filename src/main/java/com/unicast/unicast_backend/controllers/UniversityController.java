1e8f7a53dccaeae7c69611627999a704c92c4b35
Adrian <adriansamatan@gmail.com>
2019-05-03 20:26:28 +0200

    university post (con jose)

--- src/main/java/com/unicast/unicast_backend/controllers/UniversityController.java
+++ src/main/java/com/unicast/unicast_backend/controllers/UniversityController.java
@@ -1,43 +1,63 @@
 package com.unicast.unicast_backend.controllers;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import com.unicast.unicast_backend.assemblers.UniversityResourceAssembler;
 import com.unicast.unicast_backend.persistance.model.University;
 import com.unicast.unicast_backend.persistance.repository.UniversityRepository;
+import com.unicast.unicast_backend.s3handlers.S3ImageHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RepositoryRestController
+@RestController
 public class UniversityController {
 
-    private final UniversityRepository repo;
+    @Autowired
+    private UniversityRepository universityRepository;
 
     @Autowired
-    public UniversityController(UniversityRepository u){
-        this.repo = u;
+    private UniversityResourceAssembler universityAssembler;
+
+    @Autowired
+    private S3ImageHandler s3ImageHandler;
+
+    @PostMapping(value = "/api/universities/add", produces = "application/json", consumes = "multipart/form-data")
+    public ResponseEntity<?> addNewUniversity(@RequestParam("name") String name, @RequestPart("photo") MultipartFile photo) {
+
+        try {
+            URI photoURL = s3ImageHandler.uploadFile(photo);
+
+            University university = new University();
+            university.setName(name);
+            university.setPhoto(photoURL);
+
+            universityRepository.save(university);
+
+            Resource<University> resourceUniversity = universityAssembler.toResource(university);
+
+            return ResponseEntity.created(new URI(resourceUniversity.getId().getHref()))
+                    .body(resourceUniversity);
+        } catch (IOException ioE) {
+            // TODO: hacer algo
+            return ResponseEntity.badRequest().build();
+        } catch (URISyntaxException uriE) {
+            // TODO: hacer algo
+            return ResponseEntity.badRequest().build();
+        }
+        // } catch (org.springframework.dao.DataIntegrityViolationException e) {
+        // ResponseEntity<String> res = new ResponseEntity("El username ya existe",
+        // HttpStatus.BAD_REQUEST);
+        // return res;
+        // }
     }
-
-    @GetMapping(value = "/universities/ordenadas") 
-    public @ResponseBody ResponseEntity<?> getUniversities() {
-        List<University> producers = repo.findAll((Sort.by(Sort.Direction.ASC, "name")));
-
-        //
-        // do some intermediate processing, logging, etc. with the producers
-        //
-
-       /* Resources<String> resources = new Resources<String>(producers); 
-
-        resources.add(linkTo(methodOn(ScannerController.class).getProducers()).withSelfRel()); 
-
-        // add other links as needed*/
-
-        return ResponseEntity.ok(producers); 
-    }
-
 }
 
